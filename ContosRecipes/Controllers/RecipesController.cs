31fa36bb0631a128f8712e12877121f31e0e39ec
Manogna <thumuluri.manogna@ltts.com>
2022-03-28 13:32:52 +0530

    Add project files.

---
+++ ContosRecipes/Controllers/RecipesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes() {
+
+            string[] dishes = { "a", "b", "c" };
+            return dishes;
+        }
+    }
+}
---
+++ ContosRecipes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContosRecipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
